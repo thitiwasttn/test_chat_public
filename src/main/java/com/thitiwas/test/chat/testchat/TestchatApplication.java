e9be07ad89f4cd0c6faace7d92b5e28b8c70469f
Thitiwas <55071973+thitiwasttn@users.noreply.github.com>
2021-08-15 21:43:27 +0700

    first

---
+++ src/main/java/com/thitiwas/test/chat/testchat/TestchatApplication.java
@@ -0,0 +1,13 @@
+package com.thitiwas.test.chat.testchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thitiwas/test/chat/testchat/TestchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thitiwas.test.chat.testchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

bcf2219e8eb2fceebc4a950edcf8bcae2905dc15
GA007007\Admin <thitiwas.n@g-able.com>
2021-08-16 11:35:03 +0700

    up

--- src/main/java/com/thitiwas/test/chat/testchat/TestchatApplication.java
+++ src/main/java/com/thitiwas/test/chat/testchat/TestchatApplication.java
@@ -1,12 +1,15 @@
 package com.thitiwas.test.chat.testchat;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class TestchatApplication {
 
 	public static void main(String[] args) {
+		log.debug("test");
 		SpringApplication.run(TestchatApplication.class, args);
 	}
 

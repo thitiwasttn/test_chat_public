ad7548dff70eb459862eec90aadce95f7bdd86b6
GA007007\Admin <thitiwas.n@g-able.com>
2021-08-18 10:46:17 +0700

    up

---
+++ src/main/java/com/thitiwas/test/chat/testchat/config/StarterLog.java
@@ -0,0 +1,57 @@
+package com.thitiwas.test.chat.testchat.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Component
+@Slf4j
+public class StarterLog {
+
+
+    private final Environment env;
+
+    @Autowired
+    public StarterLog(Environment env) {
+        this.env = env;
+        printLogStater();
+    }
+
+    public void printLogStater() {
+        log.debug("env:: {}", env);
+        // env.get property คือ ดึงค่าจาก config file.yml
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (contextPath != null && !contextPath.equals("")) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
+    }
+}
